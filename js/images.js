7dfdcfa4e9847281f1a788fd9b80bfb9edaf85eb
KARA NADIR <105675743+daarkrain@users.noreply.github.com>
2026-05-28 15:49:03 +0200

    Update images.js

--- js/images.js
+++ js/images.js
@@ -9,5 +9,5 @@ const IMAGES = [
   "IMG_20250718_174315.jpg",
   "IMG_20251219_090504.jpg",
   "Snapchat-149121051.jpg",
-  "WhatsApp Image 2026-05-28 at 15.37.43.jpg",
+  "WhatsApp Image 2026-05-28 at 15.37.43.jpeg",
 ];
